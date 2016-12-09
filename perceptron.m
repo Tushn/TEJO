adf1be911ac0ead02fdbf690a913944f70c0d32d
Arturpg987 <artur.fhtagn@gmail.com>
2016-12-09 17:40:02 -0300

    Update perceptron.m

--- perceptron.m
+++ perceptron.m
@@ -9,6 +9,8 @@
 % - erro: error estimated
 % - y_est: 'y' estimated
 % - errors: all errors in the last training
+%
+% Note this perceptron is not ideal because it is not using error for finish training process.
 function [w, erro, y_est, errors] = perceptron(x, y, alpha, tries)
     [sizeY, sizeX] = size(x);
     w = [rand(1,sizeX) 1]';
